ff40d8046aaf257b47216e17d6784352cdbf451f
Kevin McGonigle <kevin.mcgonigle2@mail.dcu.ie>
2019-02-04 11:22:45 +0000

    Implementation of scraping for relevant text. content.js now contains code for looking at each piece of text contained within the web page and isolating those which contain reference to a list of keywords.

--- main/content.js
+++ main/content.js
@@ -1,1 +1,19 @@
-alert("if (spoilers) {block()}");
+function containsKeyWord(text){
+    let keywords = ['Russian Doll', 'Netflix'];
+    let upper_text = text.toUpperCase();
+    for (var kw of keywords){
+        if (upper_text.includes(kw.toUpperCase()))
+            return true;
+    }
+    return false;
+}
+
+let paragraphs = document.querySelectorAll('p, li, h1, h2, h3, h4, h5, h6');
+let matches = [];
+for (var p of paragraphs){
+    let text = p.innerText;
+    if (text.length > 0 && !matches.includes(text) && containsKeyWord(text))
+        matches.push(p.innerText);
+}
+
+console.log(matches);
