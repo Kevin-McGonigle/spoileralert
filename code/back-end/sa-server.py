09b9807558aa34eb0530e625fdbd3437200192e9
Kevin McGonigle <kevin.mcgonigle2@mail.dcu.ie>
2019-02-26 21:55:42 +0000

    Fixes to HTTP errors. Front end and back end now communicate as expected.

--- code/back-end/sa-server.py
+++ code/back-end/sa-server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, Response
+import ssl
 app = Flask(__name__)
 
 
@@ -9,8 +10,13 @@ def handle_post_request():
 
     # TODO: Classify the data as a spoiler or not
 
-    return "Data received"  # Temporary, in future, return classification result
+    resp = Response("Data received")
+    resp.headers["Access-Control-Allow-Origin"] = '*'
+    return resp  # Temporary, in future, return classification result
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=443, ssl_context="adhoc")
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.load_cert_chain('/root/.acme.sh/spoiler.bogpeople.com/spoiler.bogpeople.com.cer',
+                            '/root/.acme.sh/spoiler.bogpeople.com/spoiler.bogpeople.com.key')
+    app.run(host="0.0.0.0", port=443, ssl_context=context)
