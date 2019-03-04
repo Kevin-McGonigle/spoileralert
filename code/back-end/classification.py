bda5b0131c18ae527a5eb9bb5a36230852baed73
David Early <davidearly1515@gmail.com>
2019-03-04 19:34:36 +0000

    Added more training data and prediction files for Bayesian classification

--- code/back-end/classification.py
+++ code/back-end/classification.py
@@ -14,46 +14,39 @@ from pprint import pprint
 
 file = "../utils/data.csv"
 
-#cp437
 df = pd.read_csv(file, encoding="ISO-8859-1")
 
 y = df.label
 
-X_train, X_test, y_train, y_test = train_test_split(df["text"],
+X_train, X_test, y_train, y_test = train_test_split(df['text'],
                                                     y,
                                                     test_size = 0.33,
-                                                    random_state = 53)
+                                                    random_state = 17)
 
 
-count_vectorizer = CountVectorizer(stop_words="english", encoding="ISO-8859-1")
-count_train = count_vectorizer.fit_transform(X_train.values.astype(str))
-count_test = count_vectorizer.transform(X_test.values.astype(str))
-
-
-tfidf_vectorizer = TfidfVectorizer(stop_words="english", max_df=0.7)
+tfidf_vectorizer = TfidfVectorizer(stop_words="english", max_df=0.7, encoding="ISO-8859-1")
 
 # Transform the training data: tfidf_train 
 tfidf_train = tfidf_vectorizer.fit_transform(X_train.values.astype(str))
 
 # Transform the test data: tfidf_test 
-tfidf_test = tfidf_vectorizer.transform(X_test.values.astype(str))
-
+print(type(X_test.values.astype(str)))
+tfidf_test = tfidf_vectorizer.transform(["Jon Snow dies", "I'm going to have to eat every fucking chicken in this room"])
+tfidf_test = tfidf_test.astype('int')
 # Create the CountVectorizer DataFrame: count_df
-count_df = pd.DataFrame(count_train.A, columns=count_vectorizer.get_feature_names())
 
-# Create the TfidfVectorizer DataFrame: tfidf_df
-tfidf_df = pd.DataFrame(tfidf_train.A, columns=tfidf_vectorizer.get_feature_names())
 
-"""
+
 # Instantiate a Multinomial Naive Bayes classifier: nb_classifier
-nb_classifier = MultinomialNB()
+nb_classifier = MultinomialNB(alpha=0.1)
 
 # Fit the classifier to the training data
-nb_classifier.fit(count_train, y_train)
 
+nb_classifier.fit(tfidf_train, tfidf_test)
+
+x_testcv = tfidf_vectorizer.transform()
 # Create the predicted tags: pred
-print(count_test)
-pred = nb_classifier.predict(count_test)
+pred = nb_classifier.predict()
 
 # Calculate the accuracy score: score
 score = metrics.accuracy_score(y_test, pred)
@@ -63,10 +56,10 @@ print(score)
 cm = metrics.confusion_matrix(y_test, pred, labels=['SPOILER', 'NON-SPOILER'])
 print(cm)
 
-"""
+
 
 # Create a Multinomial Naive Bayes classifier: nb_classifier
-nb_classifier = MultinomialNB()
+nb_classifier = MultinomialNB(alpha=0.1)
 
 # Fit the classifier to the training data
 nb_classifier.fit(tfidf_train, y_train)
@@ -76,11 +69,10 @@ pred = nb_classifier.predict(tfidf_test)
 
 # Calculate the accuracy score: score
 score = metrics.accuracy_score(y_test, pred)
-print(score)
+#print(score)
 
-# Calculate the confusion matrix: cm
-cm = metrics.confusion_matrix(y_test, pred, labels=['SPOILER', 'NON-SPOILER'])
-print(cm)
+
+
 
 # Create the list of alphas: alphas
 alphas = np.arange(0,1,0.1)
@@ -112,9 +104,8 @@ feature_names = tfidf_vectorizer.get_feature_names()
 # Zip the feature names together with the coefficient array and sort by weights: feat_with_weights
 
 feat_with_weights = sorted(zip(nb_classifier.coef_[0], feature_names))
-
 # Print the first class label and the top 20 feat_with_weights entries
-print(class_labels[0], feat_with_weights[:20])
+#print(class_labels[0], feat_with_weights[:20])
 
 # Print the second class label and the bottom 20 feat_with_weights entries
-print(class_labels[1], feat_with_weights[-20:])
+#print(class_labels[1], feat_with_weights[-20:])
