e35aba06eb953d08ef54dd24fffd2fd9e5d226ed
Kevin McGonigle <kevin.mcgonigle2@mail.dcu.ie>
2019-03-05 11:20:17 +0000

    Overhaul of how front-end communicates with back-end and places overlays. Bug fixes to survey.

--- code/back-end/sa-server.py
+++ code/back-end/sa-server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, Response, render_template
 from random import sample
+import json
 import ssl
 import pickle
 
@@ -8,13 +9,14 @@ with open("sentences.pickle", "rb") as f:
     sentences = pickle.load(f)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["POST"])
 def handle_post_request():
-    data = request.get_data().decode("utf-8")
+    data = json.loads(request.get_data().decode("utf-8"))
 
+    indexes = [i for i in range(len(data))]
     # TODO: Classify the data as a spoiler or not
 
-    resp = Response("Data received")
+    resp = Response(json.dumps(indexes))
     resp.headers["Access-Control-Allow-Origin"] = '*'
     return resp  # Temporary, in future, return classification result
 
@@ -27,10 +29,9 @@ def load_survey():
 @app.route("/survey", methods=["POST"])
 def survey_submission():
     data = request.get_data().decode("utf-8").split("&")
-    freq = data[0][5:]  # Unused for now, left in for possible future logging
     for i in range(1, 11):
-        [id, val] = data[i].split("=")
-        sentences[int(id)][val] += 1
+        [id_num, val] = data[i].split("=") 
+        sentences[int(id_num)][val] += 1
     with open("sentences.pickle", "wb") as f:
         pickle.dump(sentences, f)
 
@@ -43,14 +44,17 @@ def min_votes():
 
 
 def pick_sentences():
-    least_votes = [(k, v["sentence"]) for k, v in sentences.items() if v["yes"] + v["no"] == min_votes()]
-    if len(least_votes) < 10:
-        least_votes += [(k, v["sentence"]) for k, v in sentences.items() if v["yes"] + v["no"] == min_votes() + 1]
+    lo = min_votes()
+    least_votes = [(k, v["sentence"]) for k, v in sentences.items() if v["yes"] + v["no"] == lo]
+    i = 1
+    while len(least_votes) < 10:
+        least_votes += [(k, v["sentence"]) for k, v in sentences.items() if v["yes"] + v["no"] == lo + i]
+        i += 1
     return enumerate(sample(least_votes, 10), 1)
 
 
 if __name__ == "__main__":
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    context.load_cert_chain('/root/.acme.sh/spoiler.bogpeople.com/spoiler.bogpeople.com.cer',
+    context.load_cert_chain('/root/.acme.sh/spoiler.bogpeople.com/fullchain.cer',
                             '/root/.acme.sh/spoiler.bogpeople.com/spoiler.bogpeople.com.key')
     app.run(host="0.0.0.0", port=443, ssl_context=context)
