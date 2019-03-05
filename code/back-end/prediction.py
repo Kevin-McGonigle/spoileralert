203961783037907b046069b4c4be78edb0afaa06
David Early <davidearly1515@gmail.com>
2019-03-05 11:04:23 +0000

    Cleaned up prediction file

--- code/back-end/prediction.py
+++ code/back-end/prediction.py
@@ -13,9 +13,6 @@ file = "../utils/data.csv"
 
 df = pd.read_csv(file, encoding="ISO-8859-1")
 
-cv = TfidfVectorizer(min_df=1, stop_words="english")
-
-
 X_train, X_test, y_train, y_test = train_test_split(df['text'],
                                                     df['label'],
                                                     test_size = 0.01,
@@ -25,17 +22,14 @@ X_train, X_test, y_train, y_test = train_test_split(df['text'],
 cv1 =TfidfVectorizer(min_df=1, stop_words="english")
 
 x_traincv = cv1.fit_transform(X_train)
-x_testcv = cv1.transform(X_test)
+#x_testcv = cv1.transform(X_test)
 text = cv1.transform(["Ned Stark is a character from Game of Thrones 2011"])
-
 
 
 y_train = y_train.astype('int')
 
 mnb = MultinomialNB(alpha=0.1)
 mnb.fit(x_traincv, y_train)
-
-x_testcv = cv1.fit_transform(X_test)
 
 pred = mnb.predict(text)
 print(pred)
