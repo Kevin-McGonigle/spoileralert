8bc1630dda1972f6aad90fababb2032747815d80
David Early <davidearly1515@gmail.com>
2019-03-07 19:56:51 +0000

    Resized popup

--- code/front-end/background.js
+++ code/front-end/background.js
@@ -2,3 +2,4 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
 	console.log("Received message");
 	sendResponse("Background got your message");
 });
+
--- code/front-end/popup.js
+++ code/front-end/popup.js
@@ -4,7 +4,19 @@
 };
 */
 
- function removeAllOverlays() {
+var isExtensionOn = true;
+
+function EnableDisableButton() {
+	var button = document.getElementById("enableDisableButton");
+	if (button.innerHTML === "Disable SpoilerAlert") 
+		isExtensionOn = false;
+	else if (button.innerHTML === "Enable SpoilerAlert")
+		isExtensionOn = true;
+	else
+		alert("Error");
+}
+
+function removeAllOverlays() {
     chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "RemoveOverlays"});
@@ -13,4 +25,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("button1").addEventListener("click", removeAllOverlays);
+  document.getElementById('enableDisableButton').addEventListener("click", disableExtension)
 });
+
+
